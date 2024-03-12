27a99221fe04210e082c3cc241d3690d8417d380
indraneel sarode <indraneel.sarode@coditas.com>
2024-03-12 15:44:05 +0530

    new python file added

---
+++ one.py
@@ -0,0 +1,51 @@
+print('ello')
+
+
+name002 = 'neel'
+
+# name! ='neel'
+
+name_23='neel'
+
+# 000name = 'adf'
+
+
+print(2+2)
+print(2+2)
+print(2+2)
+print(2+2)
+print(2+2)
+
+
+
+line1 = '********'
+line2 = '*      *'
+line3 = '*welcome*'
+
+
+print('')
+print(line1)
+print(line2)
+print(line3)
+print(line2)
+print(line1)
+
+name
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
