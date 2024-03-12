34df6fe050170bf6f772df98011cad75639d2225
indraneel sarode <indraneel.sarode@coditas.com>
2024-03-12 16:01:51 +0530

    new code

--- one.py
+++ one.py
@@ -30,20 +30,51 @@ print(line3)
 print(line2)
 print(line1)
 
-name
+print(2/3)
 
 
 
+print(24//5)
 
 
 
+print(round(24/5))
+
+print(24%5)
+
+print(2**5)
+
+
+meaning  = 42
+meaning +=1
+
+print(meaning)
+
+meaning /=10
+print(meaning)
+
+
+name = 'neel' +'sarode'
+print(name)
 
 
 
+print(42==41)
 
+print(42!=41)
 
+print(42>41)
 
+x= True
+y = False
 
+z=True
+
+print(not x)
+
+print(x and y)
+
+print(x or y)
 
 
 
